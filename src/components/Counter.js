f09bdd7bb48eeee04b79b6f83d05191c1840aa98
gentacle <gentacle00@gmail.com>
2018-10-20 15:58:29 +0900

    세부 메소드 작성 및 분리

---
+++ src/components/Counter.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react'
+import { decorate, observable, action } from 'mobx';
+import { observer } from 'mobx-react';
--- src/components/MappingTodoItem.js
+++ src/components/MappingTodoItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-export default (props)=>{
-  const { todo, index, handleCheckUpdate } = props;
-  return(
+
+//매핑 메소드
+//props로 부모의 state를 받았기 때문에 this.~~ 를 사용할 필요가 없다.
+//
+export default (props) => {
+  const { todo, index, handleCheckUpdate, handleEdit, handleRemove } = props;
+  return (
     <li>
       할일: {todo.value}<br />
-      완료 <input type="checkbox" defaultChecked={todo.checkBox} onClick={() => {handleCheckUpdate(index)}} /><br />
-      <button>수정</button>
-      <button>삭제</button>
+      완료 <input type="checkbox" defaultChecked={todo.checkBox} onClick={() => { handleCheckUpdate(index) }} /><br />
+      <button onClick={() => { handleEdit(index) }}>수정</button>
+      <button onClick={() => { handleRemove(index) }}>삭제</button>
     </li>
   )
 }
